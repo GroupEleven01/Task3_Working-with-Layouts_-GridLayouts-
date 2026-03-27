b69ae509c18653414c4fb3662e5249394b981329
Godfrey200 <godfreyjuma6969@gmail.com>
2026-03-27 21:01:35 +0300

    c3 - Updation of setup and createJpanel_1 test methods
    In the new version on line 16, 17 and 18, I have reset JPanels.JPanel1/2/3 to null in setUp, and on line 23 - 32, I have extended createJPanel_1 test to instantiate the panel and assert its properties: non-null, background set and magenta, TitledBorder titled "JPanel_1", and check that the created instance is stored in JPanels.JPanel1.

--- Grid_Module/UnitTest/GridPackage/JPanelsTest.java
+++ Grid_Module/UnitTest/GridPackage/JPanelsTest.java
@@ -13,10 +13,23 @@ class JPanelsTest {
 
     @BeforeEach
     void setUp() {
+        JPanels.JPanel1 = null;
+        JPanels.JPanel2 = null;
+        JPanels.JPanel3 = null;
     }
 
     @Test
     void createJPanel_1() {
+         JPanel Pn1 = JPanels.CreateJPanel_1();
+         TitledBorder B1 = (TitledBorder) Pn1.getBorder();
+
+         assertNotNull(Pn1);
+         assertTrue(Pn1.isBackgroundSet());
+         assertEquals(Color.magenta, Pn1.getBackground());
+         assertInstanceOf(TitledBorder.class, Pn1.getBorder());
+         assertEquals("JPanel_1", B1.getTitle());
+
+         assertSame(Pn1, JPanels.JPanel1);
     }
 
     @Test
