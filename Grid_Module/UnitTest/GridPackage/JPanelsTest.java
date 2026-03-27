a2abac14ffd4adc638dde4e3e1d512a130eb4147
Godfrey200 <godfreyjuma6969@gmail.com>
2026-03-27 20:55:40 +0300

    c2 - Add JPanelsTest unit test skeleton
    I have added Grid_Module/UnitTest/GridPackage/JPanelsTest.java as a new JUnit 5 test class.
    The file includes imports for Swing and JUnit, a setUp() method and three empty placeholder test methods (createJPanel_1/2/3) to be implemented later.

---
+++ Grid_Module/UnitTest/GridPackage/JPanelsTest.java
@@ -0,0 +1,29 @@
+package GridPackage;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JPanelsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createJPanel_1() {
+    }
+
+    @Test
+    void createJPanel_2() {
+    }
+
+    @Test
+    void createJPanel_3() {
+    }
+}
