dd18910f077dd4b90869e18ac395e59b28f5b6aa
Godfrey200 <godfreyjuma6969@gmail.com>
2026-03-31 15:07:16 +0300

    c10 - Setup, tearDown methods, and  createGridJFrame() methods update
    In the new version, I have updated the class Working_with_GridLayoutTest: add JFrame and helper fields, import Swing/AWT classes, initialize DesktopLayout in setUp, dispose created frame in tearDown, and update createGridJFrame test to assert the frame is not null and has the expected title.
    
    In the new version on line 20, I have initialized the GridFrame with the DesktopLayout.GridJFrame.
    
    In the new version on line 26 and 28, I have updated the class tearDown if condition fields.
    
    In the new version on line 40 - 46, I have renamed the setLayout to setLayoutproperties and I have added its method fields

--- Grid_Module/UnitTest/Working_with_GridLayoutTest.java
+++ Grid_Module/UnitTest/Working_with_GridLayoutTest.java
@@ -1,20 +1,50 @@
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+
+import java.awt.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Working_with_GridLayoutTest {
 
+    Working_with_GridLayout DesktopLayout;
+    JFrame GridFrame;
+
     @BeforeEach
     void setUp() {
+        DesktopLayout = new Working_with_GridLayout();
+        GridFrame = DesktopLayout.GridJFrame;
     }
 
     @AfterEach
     void tearDown() {
+
+        if (GridFrame != null) {
+            GridFrame.dispose();
+        }
     }
 
     @Test
     void createGridJFrame() {
+        GridFrame = DesktopLayout.CreateGridJFrame();
+
+        assertNotNull(GridFrame);
+        assertEquals("working with Grid layout", GridFrame.getTitle());
     }
+
+    @Test
+    void SetLayoutProperties() {
+        LayoutManager layout = GridFrame.getContentPane().getLayout();
+        assertTrue(layout instanceof GridLayout);
+
+        GridLayout grid = ((GridLayout) layout);
+        assertEquals(1, grid.getRows());
+        assertEquals(3, grid.getColumns());
+
+    }
+
 }
